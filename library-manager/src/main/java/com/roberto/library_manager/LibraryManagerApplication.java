b70324755ceea8260267261b7d72e834b67b5444
Roberto Carai <r.carai@icloud.com>
2026-03-28 23:25:26 +0100

    Enable stable Page JSON serialization

--- library-manager/src/main/java/com/roberto/library_manager/LibraryManagerApplication.java
+++ library-manager/src/main/java/com/roberto/library_manager/LibraryManagerApplication.java
@@ -6,8 +6,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Library Manager API",
